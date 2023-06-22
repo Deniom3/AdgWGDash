8263fad600604a6123357d0bee2ee36fbe556b35
Deniom <deniom3@gmail.com>
2023-06-22 19:49:05 +0300

    Добавлено дополнения правил PostUp на доступ к веб интерфейсу в сети wg.

--- install.sh
+++ install.sh
@@ -210,20 +210,6 @@ else
   exit 1
 fi
 
-# Генерация нового значения для переменной PRIVATE_KEY
-new_private_key=$(wg genkey)
-
-# Проверка наличия файла и чтение его содержимого в переменную
-config_file="./wgdash/conf/wg0.conf"
-if [[ -f "$config_file" ]]; then
-  config_content=$(cat "$config_file")
-  # Замена значения переменной PRIVATE_KEY в содержимом файла
-  new_config_content=${config_content//\$PRIVATE_KEY/$new_private_key}
-  # Запись измененного содержимого обратно в файл
-  echo "$new_config_content" > "$config_file"
-  echo "Значение PRIVATE_KEY заменено в файле $config_file"
-fi
-
 # Выводим связку логина и пароля в консоль
 echo "Ниже представлены логин и пароль для входа в AdGuardHome"
 echo -e "${GREEN}Логин: $username${NC}"
@@ -231,6 +217,37 @@ echo -e "${GREEN}Пароль: $password${NC}"
 
 # Запускаем docker-compose
 docker-compose up -d
+
+echo "Дополним правила PostUp сгенерированные автоматически."
+# Параметры для дополнения
+post_up_addition="iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o eth0 -j MASQUERADE; iptables -A INPUT -p udp -m udp --dport 10086 -j ACCEPT"
+
+# Проверка наличия файла и чтение его содержимого в переменную
+config_file="./wgdash/conf/wg0.conf"
+
+if [[ -f "$config_file" ]]; then
+  config_content=$(cat "$config_file")
+
+  # Проверка, содержит ли строка PostUp уже правило INPUT
+  if [[ $config_content =~ "iptables -A INPUT -p udp -m udp --dport 10086 -j ACCEPT" ]]; then
+    echo "Правило INPUT уже добавлено в строку PostUp."
+  else
+    # Дополнение значения параметра PostUp
+    updated_config_content="${config_content/PostUp = iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE/PostUp = iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE;$post_up_addition}"
+
+    # Дополнение значения параметра PostUp в случае, если оно не найдено
+    if [[ "$config_content" == "$updated_config_content" ]]; then
+      updated_config_content="${config_content/PostUp = iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE/PostUp = iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE;$post_up_addition}"
+    fi
+
+    # Запись измененного содержимого обратно в файл
+    echo "$updated_config_content" > "$config_file"
+
+    echo "Значение PostUp с добавленным правилом INPUT добавлено в файл $config_file"
+  fi
+else
+  echo "Файл $config_file не найден."
+fi
 
 echo ""
 echo -e "Адрес входа в веб-интерфейс WireGuard после установки: ${BLUE}http://$CURRENT_WG_HOST:10086${NC}"
